998fee1d920a64dfd455d8143fae793a503daba9
Badsha012 <badshasheikh361@gmail.com>
2025-08-19 19:43:32 -0700

    six commit

--- js/event.js
+++ js/event.js
@@ -1,1 +1,30 @@
 console.log('extension file');
+
+// option-02
+    function makeYello(params) {
+        document.body.style.backgroundColor='yellow';
+        
+    }
+ function makeRed(params) {
+    document.body.style.backgroundColor='red';
+    
+ }
+//  option -03
+const btnMake=document.getElementById('btn-blue');
+   // console.log(btnMake);
+   btnMake.onclick=function makeBlue(){
+    document.body.style.backgroundColor='blue';
+   }
+
+//    option 3 different version
+const btnMakePurpole=document.getElementById('btn-purple');
+// console.log(btnMakePurpole);
+btnMakePurpole.onclick=makePurpole();
+
+function makePurpole(params) {
+    document.body.style.backgroundColor='purple';
+    
+}
+  
+
+
